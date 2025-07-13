02897c2b119781070bbb0e1ecce23c8c7401ec3b
kannathasan-sify <kannathasan.sify@gmail.com>
2025-07-13 22:21:10 +0530

    day11_name_list

---
+++ day10_name_list.py
@@ -0,0 +1,12 @@
+# NameList.py
+
+def main():
+    # Store 5 names in a list
+    names = ["kannathasan", "Nivetha", "Samyuktha", "Ramkumar machan", "Sasikumar","sarvin","Karunya","saranya"]
+
+    print("=== Name List and Lengths ===\n")
+    for name in names:
+        print(f"Name: {name} | Length: {len(name)} characters")
+
+if __name__ == "__main__":
+    main()
